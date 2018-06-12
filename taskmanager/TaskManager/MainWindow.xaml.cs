fb7aba403636c547d1bd5a3df09d60651e72312d
Scrooge <dmitry.shebetovsky@gmail.com>
2018-06-12 19:49:35 +0300

    fix exceptions

---
+++ taskmanager/TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TaskManager
+{
+    public partial class MainWindow
+    {
+        string newProc;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TasksList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listBox = (ListBox)sender;
+
+            if (listBox.SelectedItems.Count > 0)
+            {
+                var viewModel = (ViewModel)DataContext;
+                viewModel.SelectedProcess = ((ProcessListItem)listBox.SelectedItems[0]).Process;
+            }
+        }
+
+        private void KillProcess_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var viewModel = (ViewModel)DataContext;
+                viewModel.KillSelectedProcess();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Нет прав/не выбран процесс!","Ошибка...");
+            }
+
+        }
+
+        private void OpenProc()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "all(*.*)|*.*";
+            if(dialog.ShowDialog()==true)
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = dialog.FileName;
+                process.Start();
+            }
+        }
+
+        private void OpenProcess_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (newProc != null)
+                {
+                    Process.Start(newProc);
+                    textbox.Clear();
+                    newProc = null;
+                }
+                else OpenProc();
+                    //MessageBox.Show("Напишите название процесса!");
+                    
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Ошибка...");
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            newProc = textbox.Text;
+        }
+    }
+}
---
+++ taskmanager/TaskManager/ViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Threading;
+
+namespace TaskManager
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            timer.Tick += UpdateProcesses;
+            timer.Start();
+        }
+        public Process SelectedProcess { get; set; }
+
+        public ObservableCollection<ProcessListItem> Processes { get; } = new ObservableCollection<ProcessListItem>();
+
+        public void UpdateProcesses(object sender, EventArgs e)
+        {
+            var currentIds = Processes.Select(p => p.Id).ToList();
+
+            foreach (var p in Process.GetProcesses())
+            {
+                if (!currentIds.Remove(p.Id)) 
+                {
+                    Processes.Add(new ProcessListItem(p));
+                }
+            }
+
+            foreach (var id in currentIds) 
+            {
+                var process = Processes.First(p => p.Id == id);
+                if (process.KeepAlive)
+                {
+                    Process.Start(process.ProcessName, process.Arguments);
+                }
+                Processes.Remove(process);
+            }
+        }
+
+        public void KillSelectedProcess()
+        {
+            SelectedProcess.Kill();
+        }
+    }
+}
